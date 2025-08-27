a6a09242debb2243987f61290a15e7f8a833af62
Jonathan Face <jonathan.face@protonmail.com>
2025-08-26 20:53:49 -0500

    rev

--- ui/src/App.tsx
+++ ui/src/App.tsx
@@ -7,7 +7,7 @@ function App() {
       <div>
         <img width="400px" src="https://ukstories.microsoft.com/wp-content/uploads/2016/11/minecraft1-6-4-1920x1080-png2-1.png"/>
       </div>
-      <h1><u>watch me fight</u></h1>
+      <h1><u>HELLO</u></h1>
     </>
   )
 }
